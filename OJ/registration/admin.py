b11e41fd367e9e971dbb580e08377b01188ee0d3
saket5ingh <99411565+saket5ingh@users.noreply.github.com>
2023-07-25 07:43:19 +0530

    Delete OJ directory

--- OJ/registration/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from registration.models import Problem,Submission,TestCase,Result
- 
-# Register your models here.
-admin.site.register(Problem)
-admin.site.register(Submission)
-admin.site.register(TestCase)
-admin.site.register(Result)
